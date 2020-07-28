0ac102dbef36f89ec8af3d166468ba889239bd0c
Kristian Koehntopp <kris-git@koehntopp.de>
2020-07-28 14:02:20 +0200

    mysql-commit-size experiment

---
+++ mysql-commit-size/mysql-commit-size-driver.sh
@@ -0,0 +1,11 @@
+#! /bin/bash --
+
+./mysql-commit-size.py drop
+./mysql-commit-size.py create
+for i in {5..14}
+do
+	csize=$((2 ** $i))
+	./mysql-commit-size.py truncate
+	echo "./mysql-commit-size.py fill --size=100000 --commit-size=$csize"
+	time ./mysql-commit-size.py fill --size=100000 --commit-size=$csize
+done
